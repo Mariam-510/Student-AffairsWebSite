3f949a86f10459ac4ba8b4975cac9fa58e4ad24d
Mariam Ashraf <98133355+Mariam-510@users.noreply.github.com>
2023-01-27 21:23:16 +0200

    Update settings.py

--- project2/pro2/pro2/settings.py
+++ project2/pro2/pro2/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(1_b4y!bi+(bey0kesk$d!t#_hcagvx5$$j)na9jl&olp0-j3m'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
